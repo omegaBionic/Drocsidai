c3c1b63cb87eb0c082b6f90e949435b127ce5689
youssef-t <yousseftaleb1999@gmail.com>
2020-10-20 18:52:59 +0200

    [CORE] Weather feature implemented

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import os
 
 import discord
-# openweathermap
 from dotenv import load_dotenv
+from src.weather import getWeather
 
+bot_alias = ".b "
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -22,36 +23,14 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
+    message_lower = message.content.lower()
     if message.author == client.user:
         return
-    if 'test' in message.content.lower():
+    if 'test' in message_lower:
         await message.channel.send("One message")
         print("BOT: message sent")
-
-
+    elif bot_alias + "/meteo" in message_lower:
+        await message.channel.send(getWeather(message_lower.split()[2]))
 
 client.run(TOKEN)
 
-# # bot.py
-# import os
-#
-# import discord
-# from dotenv import load_dotenv
-#
-# load_dotenv()
-# TOKEN = os.getenv('DISCORD_TOKEN')
-#
-# client = discord.Client()
-#
-# @client.event
-# async def on_ready():
-#     print(f'{client.user} has connected to Discord!')
-#
-# @client.event
-# async def on_member_join(member):
-#     await member.create_dm()
-#     await member.dm_channel.send(
-#         f'Hi {member.name}, welcome to my Discord server!'
-#     )
-#
-# client.run(TOKEN)
--- src/weather.py
+++ src/weather.py
@@ -1,16 +1,25 @@
-from pyowm.owm import OWM
-from pyowm.utils import timestamps, formatting
+import requests
 
 #api_key is available on your openweathermap's account
 api_key = "01c55848d9929f0aa1271d79bc148a78"
 
-owm = OWM(api_key)
+#get all the data about the weather
+def getWeatherall(city):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(city,api_key)
+    res = requests.get(url)
+    data = res.json()
+    return data
 
-mgr = owm.weather_manager()
-three_h_forecaster = mgr.forecast_at_place('Dijon,FR', '3h')
-for i in range(0, 24, 3):
-    tomorrow_at_five = timestamps.tomorrow(i, 0)
-    weather = three_h_forecaster.get_weather_at(tomorrow_at_five)
-    print(weather)
+#get specific data : overall weather and temperature
+def getWeather(city):
+    data = getWeatherall(city)
+    temp = data['main']['temp']
+    main_weather = data['weather'][0]['main']
+    weather = "City: {}\n\t\t{}\n\t\tTemperature : {}°C\n".format(city,main_weather,temp)
+    return weather
+
+if __name__ == '__main__':
+    data = getWeather('Dijon')
+    print(data)
 
 
