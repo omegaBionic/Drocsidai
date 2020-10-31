7c87a9cb552f0544a88f774e1757c9b53bb0b6d3
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-11-01 00:56:59 +0100

    [Core] Integrate all requests in same main file.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import os
 
+import reddit
 import discord
 from dotenv import load_dotenv
-from src.utils.weather import getWeather
+from utils.weather import Weather
 
 bot_alias = ".b "
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+reddit = reddit.Reddit()
 client = discord.Client()
+weather = Weather()
+
 
 @client.event
 async def on_ready():
@@ -23,14 +27,24 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
-    message_lower = message.content.lower()
-    if message.author == client.user:
-        return
+    if bot_alias + "youssef" in message.content.lower():
+        await message.channel.send('Hi Youssef! 🎈🎉🎉🎉🎉🎉')
+
+    message_lower = message.content.lower()  # TODO: Remove duplicate
     if 'test' in message_lower:
         await message.channel.send("One message")
         print("BOT: message sent")
-    elif bot_alias + "/meteo" in message_lower:
-        await message.channel.send(getWeather(message_lower.split()[2]))
+    elif bot_alias + "/meteo" in message_lower:  #  TODO: remove '/'
+        await message.channel.send(weather.weatherEmbed(message_lower.split()[2]))
+
+    if bot_alias + "reddit" in message.content.lower():
+        list_news = message.content.lower().split(" ")[2:]
+
+        ret_message = reddit.get_news(list_news)
+        if isinstance(ret_message, discord.Embed):
+            await message.channel.send(embed=ret_message)
+        else:
+            await message.channel.send(str(ret_message))
 
 client.run(TOKEN)
 
